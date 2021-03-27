fadb12c7d215a5b89492cd145a1a55858d3ef894
chantes5697 <42250478+chantes5697@users.noreply.github.com>
2021-03-27 14:48:57 -0600

    sd

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
+<<<<<<< HEAD
 import Home from './components/Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CustomerList from './components/CustomerList';
 import CustomerEdit from './components/CustomerEdit';
+=======
+>>>>>>> parent of 620e6c8 (react-firebase-app)
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route path='/' exact={true} component={Home}/>
-          <Route path='/customers' exact={true} component={CustomerList}/>
-          <Route path='/customers/:key' component={CustomerEdit}/>
-        </Switch>
-      </Router>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
