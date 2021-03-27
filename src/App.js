26a1140efb82e24c1d4dc44a596bc6961e9a1bbb
chantes5697 <42250478+chantes5697@users.noreply.github.com>
2021-03-27 14:46:47 -0600

    xd

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import Home from './components/Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.css';
 import CustomerList from './components/CustomerList';
 import CustomerEdit from './components/CustomerEdit';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path='/' exact={true} component={Home}/>
+          <Route path='/customers' exact={true} component={CustomerList}/>
+          <Route path='/customers/:key' component={CustomerEdit}/>
+        </Switch>
+      </Router>
+    )
+  }
 }
 
 export default App;
